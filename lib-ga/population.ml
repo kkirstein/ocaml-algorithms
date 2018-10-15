d073f9c0c7ac9b24da79a62a7fb670d9beca0eaa
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2018-10-15 13:52:37 +0200

    ! move list helpers to own lib listx

--- lib-ga/population.ml
+++ lib-ga/population.ml
@@ -9,68 +9,8 @@
  * A module for populations of travel routes
  *)
 
-(* first some helper functions *)
+open Listx
 
-(**
- * generates a list of int values with given range
- * (start inclusive, stop exlcusive)
- *)
-let rec range start stop =
-  if start < stop then start :: range (start + 1) stop
-  else []
-
-(**
- * splits the list into a parts at the given position
- *)
-let split_list n l =
-  let rec loop a b pos =
-    if pos < n then loop ((List.hd b) :: a) (List.tl b) (pos + 1)
-    else (List.rev a, b)
-  in
-  loop [] l 0
-
-(**
- * extracts a sublist of the given indices (inclusive).
- * Returns an empty list, if out of bounds.
- *)
-let sub_list start stop l =
-  let rec loop i ll =
-    match ll with
-    | [] -> ll
-    | h :: t -> if i >= start && i <= stop then h :: (loop (i + 1) t)
-      else loop (i + 1) t
-  in
-  loop 0 l
-
-(**
- * takes out the first element equal to a
- *)
-let take_out_first a l =
-  let flag = ref false in
-  List.fold_left
-    (fun acc x -> if x = a && not !flag then (flag := true; acc) else x :: acc)
-    [] l |>
-  List.rev
-
-(**
- * random sampling of list entries
- *)
-let sample_list ?n l =
-  let len = match n with
-    | None    -> List.length l
-    | Some n  -> n
-  in
-  let rec loop l i =
-    if i < len then
-      match l with
-      | [] -> []
-      | l -> let idx = Random.int (List.length l) in
-        let sample = List.nth l idx in
-        let remain = take_out_first sample l in
-        sample :: loop remain (i + 1)
-    else []
-  in
-  loop l 0
 
 (**
  * calculates the cumulative sum of the given float list
---
+++ lib-listx/listx.ml
@@ -0,0 +1,76 @@
+(* vim: set ft=ocaml sw=2 ts=2: *)
+
+(**
+ * listx.ml
+ *
+ * A module with often needed utility functions for
+ * list genertaion and processing
+ *)
+
+(**
+ * generates a list of int values with given range
+ * (start inclusive, stop exlcusive)
+ *)
+let rec range start stop =
+  if start < stop then start :: range (start + 1) stop
+  else []
+
+let%test _ = range 0 3 = [0; 1; 2]
+let%test _ = range 2 8 = [2; 3; 4; 5; 6; 7]
+let%test _ = range 2 2 = []
+let%test _ = range 4 2 = []
+
+(**
+ * splits the list into a parts at the given position
+ *)
+let split_list n l =
+  let rec loop a b pos =
+    if pos < n then loop ((List.hd b) :: a) (List.tl b) (pos + 1)
+    else (List.rev a, b)
+  in
+  loop [] l 0
+
+(**
+ * extracts a sublist of the given indices (inclusive).
+ * Returns an empty list, if out of bounds.
+ *)
+let sub_list start stop l =
+  let rec loop i ll =
+    match ll with
+    | [] -> ll
+    | h :: t -> if i >= start && i <= stop then h :: (loop (i + 1) t)
+      else loop (i + 1) t
+  in
+  loop 0 l
+
+(**
+ * takes out the first element equal to a
+ *)
+let take_out_first a l =
+  let flag = ref false in
+  List.fold_left
+    (fun acc x -> if x = a && not !flag then (flag := true; acc) else x :: acc)
+    [] l |>
+  List.rev
+
+(**
+ * random sampling of list entries
+ *)
+let sample_list ?n l =
+  let len = match n with
+    | None    -> List.length l
+    | Some n  -> n
+  in
+  let rec loop l i =
+    if i < len then
+      match l with
+      | [] -> []
+      | l -> let idx = Random.int (List.length l) in
+        let sample = List.nth l idx in
+        let remain = take_out_first sample l in
+        sample :: loop remain (i + 1)
+    else []
+  in
+  loop l 0
+
+
